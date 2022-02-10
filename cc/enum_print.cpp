304db27380f9c9682659ed2df1f8b72eec889db9
keeYuc <52500690@qq.com>
2022-02-10 20:27:20 +0800

    add 枚举打印

---
+++ cc/enum_print.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+template<typename T>
+std::ostream& operator<<(
+    typename std::enable_if<std::is_enum<T>::value,
+    std::ostream>::type& stream, const T& e)
+{
+    return stream << static_cast<typename std::underlying_type<T>::type>(e);
+}
