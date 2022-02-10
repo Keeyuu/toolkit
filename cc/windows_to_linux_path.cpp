9f2c70735882391fa4aaefb2d734f6d679490aed
keeYuc <52500690@qq.com>
2022-02-10 16:35:02 +0800

    add cpp

---
+++ cc/windows_to_linux_path.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#define MAX 1024
+using namespace std;
+void convert(char* str)
+{
+    int i = 3;
+    cout << "\n" << "/" << str[0] << "/";
+    while (true)
+    {
+        if (str[i] == '\0') { cout << endl; return; };
+        if (str[i] == '\\') cout << '/'; else cout << str[i];
+        i++;
+    }
+}
+
+
+int   main(int   argc, char* argv[])
+{
+    if (argc < 2)
+    {
+        return -1;
+    }
+    try
+    {
+        if (argv[1][1] == ':')
+        {
+            convert(argv[1]);
+        }
+    } catch (...)
+    {
+        cout << "参数错误" << endl;
+    }
+    return   0;
+}
+
+
